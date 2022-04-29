6734eba47eb85bcc6ac4208ab0f24b0fc880c662
Zachary Boelter <zboelter@gmail.com>
2022-04-29 16:12:23 -0500

    excercises

--- app.py
+++ app.py
@@ -131,26 +131,57 @@ is_in_inventory = True
 # print('replace', csv.replace(';',',').replace(':',',').split(','))
 
 #Tuples - faster Lists you can't change
-friends = ['John','Michael','Terry','Eric','Graham']
-friends_tuple = ('John','Michael','Terry','Eric','Graham')
-friends_set = {'John','Michael','Terry','Eric','Graham','Eric'}
-my_friends_set = {'Reg','Loretta','Colin','Eric','Graham'}
+# friends = ['John','Michael','Terry','Eric','Graham']
+# friends_tuple = ('John','Michael','Terry','Eric','Graham')
+# friends_set = {'John','Michael','Terry','Eric','Graham','Eric'}
+# my_friends_set = {'Reg','Loretta','Colin','Eric','Graham'}
 
-print(friends_set.difference(my_friends_set))
-print(friends_set.intersection(my_friends_set))
-print(friends_set.union(my_friends_set))
+# print(friends_set.difference(my_friends_set))
+# print(friends_set.intersection(my_friends_set))
+# print(friends_set.union(my_friends_set))
 
 
-#Sets - blazingly fast unordered Lists 
-#empty Lists
-empty_list = []
-empyt_list = list()
+# #Sets - blazingly fast unordered Lists 
+# #empty Lists
+# empty_list = []
+# empyt_list = list()
 
-#empty Tuple
-empty_tuple = ()
-empty_tuple = tuple()
+# #empty Tuple
+# empty_tuple = ()
+# empty_tuple = tuple()
 
-#empty Set
-empty_set = {} # this is wrong, this is a dictionary
-empty_set = set()
+# #empty Set
+# empty_set = {} # this is wrong, this is a dictionary
+# empty_set = set()
+
+#1. Check if ‘Eric’ and ‘John’ exist in friends
+#2. combine or add the two sets 
+#3. Find names that are in both sets
+#4. find names that are only in friends
+#5. Show only the names who only appear in one of the lists
+#6. Create a new cars-list without duplicates
+
+friends = {'John','Michael','Terry','Eric','Graham'}
+my_friends = {'Reg','Loretta','Colin','John','Graham'}
+cars =['900','420','V70','911','996','V90','911','911','S','328','900']
+
+print('Eric' in friends and 'John' in friends)
+
+
+print(my_friends.union(friends))
+print(my_friends | friends)
+
+
+print(friends.intersection(my_friends))
+print(friends & my_friends)
+
+
+print(friends.difference(my_friends))
+print(friends - my_friends)
+
+print(my_friends.symmetric_difference(friends))
+print(my_friends ^ friends)
+
+cars_no_dupl = set(cars)
+print(cars_no_dupl)
 
