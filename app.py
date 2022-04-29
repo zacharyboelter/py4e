d0143257847cb18da6b30726a72a192485623df7
Zachary Boelter <zboelter@gmail.com>
2022-04-29 12:28:47 -0500

    intial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+print('Hello World')
+
+#Create appropriate Variables for Item name, the price 
+#and how many you have in stock
+
+item_name = str('widget')
+price = float(1.89)
+in_stock = int(100)
+
+print(item_name, price, in_stock)
