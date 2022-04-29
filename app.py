b37f9cbcc5b4ad3391a24549f0707b7c040ca1c6
Zachary Boelter <zboelter@gmail.com>
2022-04-29 12:53:55 -0500

    alter strings

--- app.py
+++ app.py
@@ -12,12 +12,21 @@ in_stock = int(100)
 is_in_inventory = True
 # print(item_name, price, in_stock, is_in_inventory)
 
-a=10
-b=3
-print('Addition : ', a + b)
-print('Subtraction : ', a - b)
-print('Multiplication : ', a * b)
-print('Division (float) : ', a / b)
-print('Division (floor) : ', a // b)
-print('Modulus : ', a % b)
-print('Exponent : ', a ** b)
+# a=10
+# b=3
+# print('Addition : ', a + b)
+# print('Subtraction : ', a - b)
+# print('Multiplication : ', a * b)
+# print('Division (float) : ', a / b)
+# print('Division (floor) : ', a // b)
+# print('Modulus : ', a % b)
+# print('Exponent : ', a ** b)
+
+msg='welcome to Python 101: Strings'
+print(msg)
+print(len(msg))
+print(msg.count('o'))
+print(msg.upper())
+print(msg.lower())
+print(msg.title())
+print(msg.capitalize())
