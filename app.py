0d2ce024b0018772b5c8ab57f003cf5b1925fde3
Zachary Boelter <zboelter@gmail.com>
2022-04-29 12:36:41 -0500

    maths

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-print('Hello World')
+# print('Hello World')
 
 #Create appropriate Variables for Item name, the price 
 #and how many you have in stock
 
+from operator import truediv
+
+
 item_name = str('widget')
 price = float(1.89)
 in_stock = int(100)
+is_in_inventory = True
+# print(item_name, price, in_stock, is_in_inventory)
 
-print(item_name, price, in_stock)
+a=10
+b=3
+print('Addition : ', a + b)
+print('Subtraction : ', a - b)
+print('Multiplication : ', a * b)
+print('Division (float) : ', a / b)
+print('Division (floor) : ', a // b)
+print('Modulus : ', a % b)
+print('Exponent : ', a ** b)
