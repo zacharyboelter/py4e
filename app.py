a5e1b7f49868395ddabf1383aa78216e763b8c8d
Zachary Boelter <zboelter@gmail.com>
2022-04-29 15:39:55 -0500

    sets/tuples/dictionary

--- app.py
+++ app.py
@@ -133,5 +133,24 @@ is_in_inventory = True
 #Tuples - faster Lists you can't change
 friends = ['John','Michael','Terry','Eric','Graham']
 friends_tuple = ('John','Michael','Terry','Eric','Graham')
-print(friends[2:4])
-print(friends_tuple[2:4])
+friends_set = {'John','Michael','Terry','Eric','Graham','Eric'}
+my_friends_set = {'Reg','Loretta','Colin','Eric','Graham'}
+
+print(friends_set.difference(my_friends_set))
+print(friends_set.intersection(my_friends_set))
+print(friends_set.union(my_friends_set))
+
+
+#Sets - blazingly fast unordered Lists 
+#empty Lists
+empty_list = []
+empyt_list = list()
+
+#empty Tuple
+empty_tuple = ()
+empty_tuple = tuple()
+
+#empty Set
+empty_set = {} # this is wrong, this is a dictionary
+empty_set = set()
+
