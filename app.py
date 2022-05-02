a0c7fb53838570124645862a82eed6043df4adee
Zachary Boelter <zboelter@gmail.com>
2022-05-02 15:20:54 -0500

    so confused

--- app.py
+++ app.py
@@ -411,41 +411,90 @@
 #     print(key, value)
 
 
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!MERGING DICKS IN THREE WAYS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-
-python = {'John':35,'Eric':36,'Michael':35,'Terry':38,'Graham':37,'TerryG':34}
-holy_grail = {'Arthur':40,'Galahad':35,'Lancelot':39,'Knight of NI':40, 'Zoot':17}
-life_of_brian = {'Brian':33,'Reg':35,'Stan/Loretta':32,'Biccus Diccus':45}
+# python = {'John':35,'Eric':36,'Michael':35,'Terry':38,'Graham':37,'TerryG':34}
+# holy_grail = {'Arthur':40,'Galahad':35,'Lancelot':39,'Knight of NI':40, 'Zoot':17}
+# life_of_brian = {'Brian':33,'Reg':35,'Stan/Loretta':32,'Biccus Diccus':45}
 #membership test
 # print('Arthur' in holy_grail)
 # if 'Arthur' not in python:
 #     print('He\'s not here!')
 
-people = {}
-people1 = {}
-people2 = {}
+# people = {}
+# people1 = {}
+# people2 = {}
 
-#method 1 update
-#best used if you have only 2 dictionaries to merge
-
-
-people.update(python)
-people.update(holy_grail)
-people.update(life_of_brian)
-print(people)
-
-#method 2 comprhension 
-#best used if you have older version and need to merge more than 2 dictionaries
+# #method 1 update
+# #best used if you have only 2 dictionaries to merge
 
 
-for groups in (python, holy_grail, life_of_brian) : people1.update(groups)
-print(f'This is the comprehension method: {people1}')
+# people.update(python)
+# people.update(holy_grail)
+# people.update(life_of_brian)
+# print(people)
 
-#method 3 unpacking 3.5 or later only!!
-#best used if you have recent version and need to merge more than 2 dictionaries
+# #method 2 comprhension 
+# #best used if you have older version and need to merge more than 2 dictionaries
 
-people2 = {**python, **holy_grail, **life_of_brian}
-print(f' This is the unpacking version: {people2}')
 
-#sum of the ages of all the actors ages combined
-print('The sum of the ages is: ', sum(people.values()))
+# for groups in (python, holy_grail, life_of_brian) : people1.update(groups)
+# print(f'This is the comprehension method: {people1}')
+
+# #method 3 unpacking 3.5 or later only!!
+# #best used if you have recent version and need to merge more than 2 dictionaries
+
+# people2 = {**python, **holy_grail, **life_of_brian}
+# print(f' This is the unpacking version: {people2}')
+
+# #sum of the ages of all the actors ages combined
+# print('The sum of the ages is: ', sum(people.values()))
+
+
+
+#It’s...not really an adventure game...#Ver 1.0
+#Your village is being attacked by 'a germanic tribe' and you need to run to the stores and get the right things to save your village, and probably some good looking girl or boy you want to marry. All prices in gold pieces excl. VAT... chop chop!! ze germanz are coming!
+#The code should allow you to get 1 thing from each store and each item you get should be removed from the store inventory, then do same for next store...
+# one way to buy by typing the key 'newt' in an input box...or something
+# at end you should print the 'items' you have taken..in this version you don't have to pay for stuff or add it up
+#ver 1.2 add ability to exit a store without buying and go to next by typing 'exit', and to exit if a nonexistant item is bought(typed)
+#Add purse with 1000 gold pieces and payment for the items during or at end of code and show a message about total cost and how much gold you have left
+#ver 1.4 random bug fix, ' browser compatability', refactoring code... basically being lazy ..stop scrolling TikTok/Facebook! ;-)
+#Ver 1.5 print inventory before and after purchases as one department_store of stuff(combine inventories from all stores into one...pretend Big Biz bought all the local stores, and want constant reporting for inventory management...)
+# as in all games there is a special way to do this that actually makes money and solves the problem...can you find 'them'? Do you know why? May require knowledge of actual python 'lore'
+
+#create stores
+# freelancers = {'name':'freelancing Shop','brian': 70, 'black knight':20, 'biccus diccus':100, 'grim reaper':500, 'minstrel':-15}
+# antiques = {'name':'Antique Shop','french castle':400, 'wooden grail':3, 'scythe':150, 'catapult':75, 'german joke':5}
+# pet_shop = {'name':'Pet Shop','blue parrot':10, 'white rabbit':5, 'newt': 2}
+
+# #create an dempty shopping cart
+# cart = {}
+# #loop through stores/dicts
+# for shop in (freelancers,antiques,pet_shop) :
+#     #inputbox  to show what you can buy...capture textstring of what was bought...make lowercase
+#     buy_item = input(f'Welcome to {shop["name"]}! what do you want to buy: {shop}').lower()
+#     #update the cart
+#     cart.update({buy_item:shop.pop(buy_item)}) # use pop...
+#     buy_items = ", ".join(list(cart.keys()))
+# print(f'You Purchased {buy_items}. Today it is all free. Have a nice day of mayhem!')
+
+
+
+
+freelancers = {'name':'freelancing Shop','brian': 70, 'black knight':20, 'biccus diccus':100, 'grim reaper':500, 'minstrel':-15}
+antiques = {'name':'Antique Shop','french castle':400, 'wooden grail':3, 'scythe':150, 'catapult':75, 'german joke':5}
+pet_shop = {'name':'Pet Shop','blue parrot':10, 'white rabbit':5, 'newt': 2}
+
+#create an dempty shopping cart
+cart = {}
+#loop through stores/dicts
+
+for shop in  (freelancers, antiques, pet_shop) :
+    #inputbox  to show what you can buy...capture textstring of what was bought...make lowercase
+    buy_item = input(f'Welcome to {shop["name"]}! what do you want to buy: {shop}').lower()
+    #update the cart
+    cart.update({buy_item:shop.pop(buy_item)}) # use pop...
+    #set the string to variable to clean it up
+    buy_items = ", ".join(list(cart.keys()))
+print(f'You Purchased {buy_items} Today it is all free. Have a nice day of mayhem!')
