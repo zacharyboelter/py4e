44c9a82e0a5b01e87a541090c143f2892ee6c008
Zachary Boelter <zboelter@gmail.com>
2022-05-02 13:39:23 -0500

    enumerate

--- app.py
+++ app.py
@@ -369,3 +369,30 @@ names.extend(names1)
 
 for name in names:
     print(f'{name.title()}! You are invited to the party on Saturday!')
+
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#  Enumeration - Enumerate
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+print('python101 - Enumerate')
+friends = ['Brian', 'Judith', 'Reg', 'Loretta', 'Colin']
+efriends = [(51,'Brian'), (52,'Judith'), (53,'Reg'), (54,'Loretta'), (55,'Colin')]
+
+#i = 51
+#for friend in friends:
+#    print(i, friend)
+#    i = i +1 # += 1
+for num, friend in enumerate(friends,51):
+    print(num, friend)
+for friend in enumerate(friends,51):
+    print(friend)
+for friend in enumerate(enumerate(friends,51),-100):
+    print(friend)   
+for num, letter in enumerate('python',start = 5):
+    print(num, letter)
+print(type(enumerate(friends)))
+print(list(enumerate(friends)))
